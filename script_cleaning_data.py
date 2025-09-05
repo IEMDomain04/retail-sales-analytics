e53db51ab82e1f7863a35fd96ad64a4733799654
IEMDomain04 <emmanman0011@gmail.com>
2025-09-05 22:06:37 +0800

    gitignore added

--- script_cleaning_data.py
+++ script_cleaning_data.py
@@ -1,7 +1,7 @@
 import pandas as pd # type: ignore
 
 # ----------------------------- 
-# Step 1: Load dataset
+# Load dataset
 # -----------------------------
 df = pd.read_csv("sales_data_sample.csv", encoding="latin1")
 
