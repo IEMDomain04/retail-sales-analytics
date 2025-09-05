461d693166c33cff7361910813976e3778394c07
IEMDomain04 <emmanman0011@gmail.com>
2025-09-05 23:20:59 +0800

    dcs[file naming]: added a note

--- data_cleaning_script.py
+++ data_cleaning_script.py
@@ -66,6 +66,6 @@ df.loc[df["SALES"] > upper_bound, "SALES"] = upper_bound
 
 
 # -----------------------------
-# Call the function: Save cleaned dataset
+# Call the function: Save cleaned dataset... Kindly change the filename if needed to increment for the version..
 # -----------------------------
 save_cleaned_data(df, "cleaned_sales_data_V3.csv")
